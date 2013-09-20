8aba5bf5c84e2492240acec7ade1c518d84ee6ae
Sam <sami.mirimiri@live.com>
2013-09-20 02:05:28 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tut2::Application.config.secret_token = '6d1c9052877dfc2f82c9d4e1d3430ec66f6c7e29a015bded955371764d5a05556b4f302e7a454f56cda805dafd110b79cdec62ea3086589a745fda73f56665c8'
